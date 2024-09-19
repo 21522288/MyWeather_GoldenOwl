82f485bbcadae8266533c4d850023e4720c4e2d7
21522288 <116091982+21522288@users.noreply.github.com>
2024-09-19 14:47:54 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,11 @@ import { Notification } from './notification.entity';
 @Module({
   imports: [  TypeOrmModule.forRoot({
     type: 'postgres',
-    host: 'dpg-crlrourv2p9s73e3e1qg-a',  
+    host: 'localhost',  
     port: 5432,         
-    username: 'weather_db_b5y7_user', 
-    password: 'LWWetqWV8hUV5Skqci6CYOKjSZJQP4KE', 
-    database: 'weather_db_b5y7', 
+    username: 'postgres', 
+    password: 'password', 
+    database: 'weather_db', 
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
     synchronize: true, 
   }),
